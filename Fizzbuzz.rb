f073ceb1bb6206a9c8a26415f0a8cc0ae70ea3fc
Alhuar <alfonsohuescar@gmail.com>
2016-03-09 22:55:33 +0100

    Solución final al ejercicio Fizzbuzz

---
+++ Fizzbuzz.rb
@@ -0,0 +1,21 @@
+1.upto(100) do |num|
+num_st = num.to_s
+result = ""
+  	
+	if num % 3 == 0
+  		result = result + "Fizz"
+  	end
+  	if num % 5 == 0
+		result = result + "Buzz"
+	end
+	if num % 3 != 0 && num % 5 != 0 && num_st[0] != "1"
+	result = num
+	end
+
+
+	if num_st[0] == "1"
+		result << "Bang"
+	end
+
+puts result
+end
