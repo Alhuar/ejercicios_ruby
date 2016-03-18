a421c706bac221876663a2e7ba5bc02e95177850
Alhuar <alfonsohuescar@gmail.com>
2016-03-18 18:20:17 +0100

    Update cc.rb

--- cc.rb
+++ cc.rb
@@ -1,3 +1,27 @@
+#primera iteración
+
+def solve_cipher(input)
+	 arr_input = input.split("")
+	 arr_input.each do |letter|
+	  
+	  letraencriptada = letter.ord
+	  if letraencriptada == 97
+	  	letraencriptada = 122
+	  	print letraencriptada.chr
+	  else 
+	  print (letter.ord-1).chr
+	  end
+	 
+	end
+
+end
+
+
+  
+# Segunda iteración
+
+
+ solve_cipher("ifmmpa")
 
 a = 'Buenas tardes'
 
